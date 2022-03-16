e22f8f7c51ae4fbb3546df46dd4cd4c637891ade
N1ckn1ght <n1ckn1ght@protonmail.com>
2022-03-16 17:06:26 +0800

    gson fix; img src fix

--- app/src/main/java/com/example/currentweatherdatabinding/MainActivity.kt
+++ app/src/main/java/com/example/currentweatherdatabinding/MainActivity.kt
@@ -84,12 +84,15 @@ class MainActivity : AppCompatActivity() {
                            val main: WeatherMain, val visibility: Long, val wind: WeatherWind,
                            val clouds: WeatherClouds, val dt: Long, val sys: WeatherSys,
                            val timezone: Long, val id: Long, val name: String, val cod: Int)
+
     data class Coord(val lon: Double, val lat: Double)
     data class WeatherArray(val id: Int, val main: String, val description: String, val icon: String)
     data class WeatherMain(val temp: String, val feels_like: Double,
                            val temp_min: Double, val temp_max: Double,
-                           val pressure: Int, val humidity: Int)
-    data class WeatherWind(val speed: String, val deg: Int)
+                           val pressure: Int, val humidity: Int,
+                           val gust: Double = 0.0, val sea_level: Int = 0, val grnd_level:Int = 0)
+    data class WeatherWind(val speed: String, val deg: Int,
+                           val gust: Double = 0.0)
     data class WeatherClouds(val all: Int)
     data class WeatherSys(val type: Int, val id: Int, val country: String, val sunrise: Long, val sunset: Long)
 }
